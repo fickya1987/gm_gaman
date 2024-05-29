19b2017fde692db8610a41c781669519e97d6465
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-05-29 22:25:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,10 +83,10 @@ elif tabs == 'Akses Pasar UMKM':
     #Extracted unique tags from the data so far filtered
     unique_tags_value = list(set(combined_list))
 
-    search_value_tags = st.multiselect("Select the top 10 fields of science of your expertises: ", sorted(unique_tags_value), max_selections=10, format_func=lambda x: 'Select an option' if x == '.' else x)
+    search_value_tags = st.multiselect("Pilih Sektor Usaha: ", sorted(unique_tags_value), max_selections=10, format_func=lambda x: 'Select an option' if x == '.' else x)
     tags_data = language_data[language_data["fields of science"].apply(lambda x: any(tag in x for tag in search_value_tags))]
 
-    columns_to_display = ["Nama UMKM", "Deskripsi Keterangan", "Alamat", "Nama Kontak", "Nomor Kontak"]
+    columns_to_display = ["Name of Project", "project description", "keywords", "government name of contact", "government contact email"]
 
     #Dictionary to map original column names to display names
     display_names = {
